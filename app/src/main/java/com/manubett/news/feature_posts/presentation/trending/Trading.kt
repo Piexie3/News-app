569d48f68b5f97f0894c78e518dc02a2be3e333c
piexie3 <emmanuelkbett3@gmail.com>
2023-03-11 17:13:42 +0300

    initial commit

--- app/src/main/java/com/manubett/news/feature_posts/presentation/trending/Trading.kt
+++ app/src/main/java/com/manubett/news/feature_posts/presentation/trending/Trading.kt
@@ -3,7 +3,6 @@ package com.manubett.news.feature_posts.presentation.trending
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,24 +15,24 @@ import com.manubett.news.navigation.BottomNavItem
 import com.manubett.news.navigation.BottomNavMenu
 
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TrendingScreen(
     navController: NavController
 ) {
-    Scaffold(bottomBar = {
-        BottomAppBar(
-            modifier = Modifier
-                .height(48.dp),
-            containerColor = if (isSystemInDarkTheme())
-                Color.Black.copy(.24f) else
-                Color.White.copy(
-                    .24f
-                ),
-        ) {
-            BottomNavMenu(selectedItem = BottomNavItem.TREND, navController)
-        }
-    }) { PaddingValues ->
+    Scaffold(
+        bottomBar = {
+            BottomAppBar(
+                modifier = Modifier
+                    .height(48.dp),
+                containerColor = if (isSystemInDarkTheme())
+                    Color.Black.copy(.24f) else
+                    Color.White.copy(
+                        .24f
+                    ),
+            ) {
+                BottomNavMenu(selectedItem = BottomNavItem.TREND, navController)
+            }
+        }) { PaddingValues ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
